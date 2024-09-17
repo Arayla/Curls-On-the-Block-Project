bd57927fbad58dff98085af4c9bd3df806f799b9
drew <drsm1370@colorado.edu>
2024-09-17 10:21:00 -0600

    example for basic frontend rendering

--- client/src/components/sample-component.tsx
+++ client/src/components/sample-component.tsx
@@ -1,25 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-async function callServer(): Promise<void> {
-  try {
-    const response = await axios.get("http://localhost:8000/test", {
-      params: {
-        table: "sample",
-      },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error("Error calling server:", error);
-  }
+interface Data {
+  id: number;
+  name: string;
 }
 
 export function SampleComponent(): JSX.Element {
+  let [loading, setLoading] = useState(true);
+
+  let [data, setData] = useState<Data[]>([]);
+
+  async function callServer(): Promise<void> {
+    try {
+      const response = await axios.get("http://localhost:8000/test", {
+        params: {
+          table: "sample",
+        },
+      });
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error calling server:", error);
+    }
+  }
+
   useEffect(() => {
     callServer();
   }, []);
 
-  return <div>This is a sample component</div>;
+  if (loading) return <div>Loading...</div>;
+
+  let names: string = "";
+  for (let i = 0; i < data.length; i++) {
+    names += "{id: " + data[i].id + ", name: " + data[i].name + "}\n";
+  }
+
+  return (
+    <>
+      <div>This is a sample component</div>
+      <div>{names}</div>
+    </>
+  );
 }
 
 export default SampleComponent;
