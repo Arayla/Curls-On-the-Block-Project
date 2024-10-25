34202e985235e639d8c720d152032f88f0844fca
drew <drsm1370@colorado.edu>
2024-10-25 13:40:09 -0600

    div for styling later

--- client/src/components/SearchParamForm.tsx
+++ client/src/components/SearchParamForm.tsx
@@ -46,134 +46,133 @@ export const SearchParamForm: React.FC<SearchFormProps> = ({
 
   return (
     <>
-      <InputField
-        label="Select Porosity:"
-        listItems={[
-          "Select a value",
-          "Not very",
-          "Somewhat",
-          "Average",
-          "Above average",
-          "Very",
-        ]}
-        selectedOption={searchParams.porosity}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, porosity: newVal });
-        }}
-      />
-      <InputField
-        label="Select Courseness:"
-        listItems={[
-          "Select a value",
-          "Not very",
-          "Somewhat",
-          "Average",
-          "Above average",
-          "Very",
-        ]}
-        selectedOption={searchParams.courseness}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, courseness: newVal });
-        }}
-      />
-      <InputField
-        label="Select Thickness:"
-        listItems={[
-          "Select a value",
-          "Not very",
-          "Somewhat",
-          "Average",
-          "Above average",
-          "Very",
-        ]}
-        selectedOption={searchParams.thickness}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, thickness: newVal });
-        }}
-      />
-      <InputField
-        label="Select Curl Type:"
-        listItems={[
-          "Select a value",
-          "2A",
-          "2B",
-          "2C",
-          "3A",
-          "3B",
-          "3C",
-          "4A",
-          "4B",
-          "4C",
-        ]}
-        selectedOption={searchParams.curlType}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, curlType: newVal });
-        }}
-      />
-      <InputField
-        label="Select Length:"
-        listItems={[
-          "Select a length",
-          "Short",
-          "Somewhat short",
-          "Average length",
-          "Long",
-          "Very long",
-        ]}
-        selectedOption={searchParams.length}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, length: newVal });
-        }}
-      />
-      <InputField
-        label="Select a Search Type:"
-        listItems={["Select a search type", "Style based", "Product based"]}
-        selectedOption={searchParams.searchType}
-        onChange={(newVal: number) => {
-          setSearchParams({ ...searchParams, searchType: newVal });
-        }}
-      />
-
-      {/* Search by Category */}
-      {searchParams.searchType === SEARCH_CATEGORIES && (
+      <div className="search-param-form">
         <InputField
-          label="Select Category:"
-          listItems={categories}
-          selectedOption={categories.indexOf(searchParams.category)}
+          label="Select Porosity:"
+          listItems={[
+            "Select a value",
+            "Not very",
+            "Somewhat",
+            "Average",
+            "Above average",
+            "Very",
+          ]}
+          selectedOption={searchParams.porosity}
           onChange={(newVal: number) => {
-            setSearchParams({
-              ...searchParams,
-              category: categories[newVal],
-            });
+            setSearchParams({ ...searchParams, porosity: newVal });
           }}
         />
-      )}
-
-      {/* Search by Styles */}
-      {searchParams.searchType === SEARCH_STYLES && (
         <InputField
-          label="Select Style:"
-          listItems={styles}
-          selectedOption={styles.indexOf(searchParams.style)}
-          onChange={(newIdx: number) => {
-            setSearchParams({ ...searchParams, style: styles[newIdx] });
+          label="Select Courseness:"
+          listItems={[
+            "Select a value",
+            "Not very",
+            "Somewhat",
+            "Average",
+            "Above average",
+            "Very",
+          ]}
+          selectedOption={searchParams.courseness}
+          onChange={(newVal: number) => {
+            setSearchParams({ ...searchParams, courseness: newVal });
           }}
         />
-      )}
-      {searchParams.style !== "" && (
         <InputField
-          label="Select Category Combo:"
-          listItems={combos}
-          selectedOption={combos.indexOf(searchParams.category)}
-          onChange={(newIdx: number) => {
-            setSearchParams({ ...searchParams, category: combos[newIdx] });
+          label="Select Thickness:"
+          listItems={[
+            "Select a value",
+            "Not very",
+            "Somewhat",
+            "Average",
+            "Above average",
+            "Very",
+          ]}
+          selectedOption={searchParams.thickness}
+          onChange={(newVal: number) => {
+            setSearchParams({ ...searchParams, thickness: newVal });
           }}
         />
-      )}
-
-      <button type="submit" onClick={handleSubmit}>
-        Search
-      </button>
+        <InputField
+          label="Select Curl Type:"
+          listItems={[
+            "Select a value",
+            "2A",
+            "2B",
+            "2C",
+            "3A",
+            "3B",
+            "3C",
+            "4A",
+            "4B",
+            "4C",
+          ]}
+          selectedOption={searchParams.curlType}
+          onChange={(newVal: number) => {
+            setSearchParams({ ...searchParams, curlType: newVal });
+          }}
+        />
+        <InputField
+          label="Select Length:"
+          listItems={[
+            "Select a length",
+            "Short",
+            "Somewhat short",
+            "Average length",
+            "Long",
+            "Very long",
+          ]}
+          selectedOption={searchParams.length}
+          onChange={(newVal: number) => {
+            setSearchParams({ ...searchParams, length: newVal });
+          }}
+        />
+        <InputField
+          label="Select a Search Type:"
+          listItems={["Select a search type", "Style based", "Product based"]}
+          selectedOption={searchParams.searchType}
+          onChange={(newVal: number) => {
+            setSearchParams({ ...searchParams, searchType: newVal });
+          }}
+        />
+        {/* Search by Category */}
+        {searchParams.searchType === SEARCH_CATEGORIES && (
+          <InputField
+            label="Select Category:"
+            listItems={categories}
+            selectedOption={categories.indexOf(searchParams.category)}
+            onChange={(newVal: number) => {
+              setSearchParams({
+                ...searchParams,
+                category: categories[newVal],
+              });
+            }}
+          />
+        )}
+        {/* Search by Styles */}
+        {searchParams.searchType === SEARCH_STYLES && (
+          <InputField
+            label="Select Style:"
+            listItems={styles}
+            selectedOption={styles.indexOf(searchParams.style)}
+            onChange={(newIdx: number) => {
+              setSearchParams({ ...searchParams, style: styles[newIdx] });
+            }}
+          />
+        )}
+        {searchParams.style !== "" && (
+          <InputField
+            label="Select Category Combo:"
+            listItems={combos}
+            selectedOption={combos.indexOf(searchParams.category)}
+            onChange={(newIdx: number) => {
+              setSearchParams({ ...searchParams, category: combos[newIdx] });
+            }}
+          />
+        )}
+        <button type="submit" onClick={handleSubmit}>
+          Search
+        </button>
+      </div>
     </>
   );
 };
